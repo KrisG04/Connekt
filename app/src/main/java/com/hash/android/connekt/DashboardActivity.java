ee4945e7ae081386230f5a2c96a0a98a0d22a2d6
banerjeesumitabha <banerjeesumitabha18@gmail.com>
2017-09-02 15:55:03 +0530

    Activity home created

--- app/src/main/java/com/hash/android/connekt/DashboardActivity.java
+++ app/src/main/java/com/hash/android/connekt/DashboardActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class DashboardActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
