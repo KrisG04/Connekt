acd4eaccf44bfd309388ef00429ec0815273a488
sapt3 <saptarshi.ironmaiden@gmail.com>
2017-09-03 09:24:36 +0530

    Login screen updated

---
+++ app/src/main/java/com/hash/android/connekt/FacebookLogin.java
@@ -0,0 +1,255 @@
+package com.hash.android.connekt;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FacebookLogin extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 12344;
+    private static final String TAG = FacebookLogin.class.getSimpleName();
+    private static AccessToken token;
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+
+    Button loginWithFacebook;
+    private ProgressDialog pd;
+    private PreferenceManager mPrefs;
+    private EditText userNameET;
+    private TextView urlTextView;
+    private String department;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_login);
+        TextView text;
+        text = (TextView) findViewById(R.id.connektTextView);
+        mPrefs = new PreferenceManager(this);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "pacifico.ttf");
+        loginWithFacebook = (Button) findViewById(R.id.loginWithFacebookButton);
+        loginWithFacebook.setOnClickListener(this);
+        text.setTypeface(custom_font);
+        pd = new ProgressDialog(FacebookLogin.this);
+        Spinner mSpinner = (Spinner) findViewById(R.id.spinner);
+        userNameET = (EditText) findViewById(R.id.usernameTextView);
+        urlTextView = (TextView) findViewById(R.id.urlUserNameTextView);
+
+        mSpinner.setAdapter(getArtsDepartmentAdapter());
+
+        userNameET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                urlTextView.setText(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                department = adapterView.getItemAtPosition(i).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.loginWithFacebookButton) {
+            //Start login flow
+            signIn();
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        final FirebaseUser user = auth.getCurrentUser();
+        if (user != null && new PreferenceManager(this).isFlowCompleted()) {
+            Intent i = new Intent(FacebookLogin.this, DashboardActivity.class);
+            FacebookLogin.this.overridePendingTransition(0, 0);
+            startActivity(i);
+            finish();
+        }
+    }
+
+    private void fetchGraphData(AccessToken token) {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null && token != null) {
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        AccessToken.getCurrentAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                try {
+                                    String link = object.getString("link");
+                                    String gender = object.getString("gender");
+                                    JSONObject profilePhoto = object.getJSONObject("picture");
+                                    JSONObject data = profilePhoto.getJSONObject("data");
+                                    String photoURL = data.getString("url");
+                                    JSONObject cover = object.getJSONObject("cover");
+                                    String coverURL = cover.getString("source");
+                                    String name = object.getString("name");
+                                    mPrefs.setLink(link);
+                                    mPrefs.setGender(gender);
+                                    mPrefs.setCoverURL(coverURL);
+                                    mPrefs.setPhotoURL(photoURL);
+                                    mPrefs.setName(name);
+                                    mPrefs.saveUser();
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "name, gender, email, link, cover, picture.type(large)");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+    }
+
+
+    private SpinnerAdapter getArtsDepartmentAdapter() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Jadavpur University");
+        list.add("IIEST Shibpur");
+        list.add("IIT Kharagpur");
+        list.add("NIT Durgapur");
+
+
+        return new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, list);
+    }
+
+
+    private void signIn() {
+
+        //Create an IDPConfig for facebook
+        AuthUI.IdpConfig facebookIdp = new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER)
+                .setPermissions(Arrays.asList("public_profile", "email"))
+                .build();
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(
+                                        facebookIdp
+                                )
+                        )
+                        .build()
+                , RC_SIGN_IN
+        );
+        if (!pd.isShowing()) {
+            pd.setMessage("Creating your brand new account!");
+            pd.show();
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+                token = AccessToken.getCurrentAccessToken();
+                new PreferenceManager(FacebookLogin.this).setFacebookToken(token.toString());
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+
+                    fetchGraphData(token);
+                    mPrefs.setUID(user.getUid());
+                    mPrefs.setEmail(user.getEmail());
+                    if (user.getPhotoUrl() != null)
+                        mPrefs.setPhotoURL(user.getPhotoUrl().toString());
+                    FirebaseAuth.getInstance().signOut();
+                    //TODO: Start an intent to go to login activity
+
+                } else {
+                    Toast.makeText(this, "Failed to sign in", Toast.LENGTH_SHORT).show();
+                }
+                return;
+
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this, "Sign in cancelled!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, "Unknown Error", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+
+
+            Toast.makeText(this, "Unknown Error", Toast.LENGTH_SHORT).show();
+        }
+
+        pd.hide();
+        pd.dismiss();
+    }
+}
---
+++ app/src/main/java/com/hash/android/connekt/User.java
@@ -0,0 +1,251 @@
+package com.hash.android.connekt;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
+public class User implements Parcelable {
+
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+    public Map<String, Boolean> updatesKey = new HashMap<>();
+    private Map<String, Boolean> bookmarks = new HashMap<>();
+    private String name;
+    private String phoneNumber;
+    private String university;
+    private String gender;
+    private String link;
+    private String coverURL;
+    private String photoURL;
+    private String faculty;
+    private String department;
+    private String yearOfPassing;
+    private String UID;
+    private boolean targetPromo;
+    private String about;
+    private String birthday;
+    private String email;
+    private boolean isPrivate = false;
+    private String notificationKey;
+
+    protected User(Parcel in) {
+        name = in.readString();
+        phoneNumber = in.readString();
+        university = in.readString();
+        gender = in.readString();
+        link = in.readString();
+        coverURL = in.readString();
+        photoURL = in.readString();
+        faculty = in.readString();
+        department = in.readString();
+        yearOfPassing = in.readString();
+        UID = in.readString();
+        targetPromo = in.readByte() != 0;
+        about = in.readString();
+        birthday = in.readString();
+        email = in.readString();
+        isPrivate = in.readByte() != 0;
+        notificationKey = in.readString();
+    }
+
+    public User(String name, String phoneNumber, String university, String gender, String link, String coverURL, String photoURL, String faculty, String department, String yearOfPassing, String UID, boolean targetPromo, String about, String birthday, String email, boolean isPrivate, String notificationKey) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.university = university;
+        this.gender = gender;
+        this.link = link;
+        this.coverURL = coverURL;
+        this.photoURL = photoURL;
+        this.faculty = faculty;
+        this.department = department;
+        this.yearOfPassing = yearOfPassing;
+        this.UID = UID;
+        this.targetPromo = targetPromo;
+        this.about = about;
+        this.birthday = birthday;
+        this.email = email;
+        this.isPrivate = isPrivate;
+        this.notificationKey = notificationKey;
+    }
+
+    public User() {
+    }
+
+    public String getNotificationKey() {
+        return notificationKey;
+    }
+
+    public void setNotificationKey(String notificationKey) {
+        this.notificationKey = notificationKey;
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public void setPrivate(boolean aPrivate) {
+        isPrivate = aPrivate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getCoverURL() {
+        return coverURL;
+    }
+
+    public void setCoverURL(String coverURL) {
+        this.coverURL = coverURL;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getYearOfPassing() {
+        return yearOfPassing;
+    }
+
+    public void setYearOfPassing(String yearOfPassing) {
+        this.yearOfPassing = yearOfPassing;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
+
+    public boolean isTargetPromo() {
+        return targetPromo;
+    }
+
+    public void setTargetPromo(boolean targetPromo) {
+        this.targetPromo = targetPromo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(phoneNumber);
+        parcel.writeString(university);
+        parcel.writeString(gender);
+        parcel.writeString(link);
+        parcel.writeString(coverURL);
+        parcel.writeString(photoURL);
+        parcel.writeString(faculty);
+        parcel.writeString(department);
+        parcel.writeString(yearOfPassing);
+        parcel.writeString(UID);
+        parcel.writeByte((byte) (targetPromo ? 1 : 0));
+        parcel.writeString(about);
+        parcel.writeString(birthday);
+        parcel.writeString(email);
+        parcel.writeByte((byte) (isPrivate ? 1 : 0));
+        parcel.writeString(notificationKey);
+    }
+}
